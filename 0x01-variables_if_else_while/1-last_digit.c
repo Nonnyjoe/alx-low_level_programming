36671992233f051842dbf490c6dedb0441dfc790
Idogwu <idogwuchinonso1@gmail.com>
2022-06-03 14:59:26 -0700

    this

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,15 +16,15 @@ n = rand() - RAND_MAX / 2;
 last_digit = n % 10;
 if (last_digit > 5)
 {
-printf("Last digit of %n is %d and is greater that 5\n", n,  last_digit);
+printf("Last digit of %d is %d and is greater that 5\n", n,  last_digit);
 }
 else if (last_digit == 0)
 {
-printf("Last digit of %n is %d and is 0\n", n, last_digit);
+printf("Last digit of %d is %d and is 0\n", n, last_digit);
 }
 else if (last_digit < 6 && last_digit != 0)
 {
-printf("Last digit of %n is %d and is less that 6 and not 0\n", n, last_digit);
+printf("Last digit of %d is %d and is less that 6 and not 0\n", n, last_digit);
 }
 
 return (0);
