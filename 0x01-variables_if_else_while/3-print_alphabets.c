070ffc4eff6302dcc8d9ddc94952d15830451791
Idogwu <idogwuchinonso1@gmail.com>
2022-06-03 16:35:03 -0700

    omoh

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print all the alphabets in small caps
+ * Return: alphabets
+ */
+int main(void)
+{
+
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+for (ch = 'A'; ch <= 'Z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
