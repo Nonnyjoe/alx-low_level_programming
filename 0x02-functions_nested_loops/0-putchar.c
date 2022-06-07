31c37af874af2e445362f607336ef32cf38bd9a4
Idogwu <idogwuchinonso1@gmail.com>
2022-06-07 14:35:34 -0700

    lets test it

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints _putchar
+ * Description: A code that prints _putchar
+ * Return: Return zero
+ */
+int main(void)
+{
+char msg = "_putchar";
+putchar(msg);
+putchar('\n');
+return (0);
+}
