cc2587374d0cf960bca31e3da9a5beb583f217ac
Idogwu <idogwuchinonso1@gmail.com>
2022-06-09 14:31:14 -0700

    now

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - prints numbers 10 times.
+ * Return: return 0
+ */
+
+void more_numbers(void)
+{
+int a = 0;
+while (a <= 10)
+{
+int b;
+for (b = 0; b <= 14; b++)
+{
+_putchar(b);
+_putchar('\n');
+}
+a++
+}
+}
