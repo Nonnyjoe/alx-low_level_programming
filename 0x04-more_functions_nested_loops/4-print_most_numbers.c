9e89d99c8ae1f72d1e8b12caf55fe2e7b29d8aa2
Idogwu <idogwuchinonso1@gmail.com>
2022-06-09 14:03:20 -0700

    nest

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - print numbers from 1-9 except 2 & 4
+ * Return: none
+ */
+
+void print_most_numbers(void)
+{
+int a;                                                                                                                                  
+for (a = 0; a <= 9; a++)
+{
+while (a != 2 && a != 4)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
+}
