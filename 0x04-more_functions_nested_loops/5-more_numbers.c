247c9e3506a1dba958f8ac680ae14a341495bf37
Idogwu Emmanuel Chinonso <104998136+Nonnyjoe@users.noreply.github.com>
2022-06-09 14:34:32 -0700

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,15 +6,16 @@
 
 void more_numbers(void)
 {
-int a = 0;
-while (a <= 10)
-{
-int b;
-for (b = 0; b <= 14; b++)
-{
-_putchar(b);
-_putchar('\n');
-}
-a++;
-}
-}
+	int a;
+	int b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			if (b > 9)
+				_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+		}
+	_putchar('\n');
+	}
