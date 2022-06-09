bb62fe8da2f308969718a48885ba06d449a9edcd
Idogwu <idogwuchinonso1@gmail.com>
2022-06-09 12:03:33 -0700

    first commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - checks if the text is upper or lower
+ * Return: returns zero
+ */
+
+int _isupper(int c)
+{
+if (c > 64 && c < 91)
+{
+return(1);
+}
+else 
+{
+return(0);
+}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this is a continuation of function and nested loops
